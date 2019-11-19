f8da3ca34bdab2a19ffc9d5816f07776e13e03fc
Narender Bisht <narender.bisht84@gmail.com>
2019-11-19 11:49:14 +0530

    sample spring boot app

---
+++ src/main/java/fco/svc/node/MicroserviceApplication.java
@@ -0,0 +1,16 @@
+package fco.svc.node;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author narbisht
+ *
+ */
+@SpringBootApplication
+public class MicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceApplication.class, args);
+	}
+}
---
+++ src/main/java/fco/svc/node/controller/HelloWorldController.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package fco.svc.node.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+
+/**
+ * @author narbisht
+ *
+ */
+@RestController
+@RequestMapping(path="/sample", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+public class HelloWorldController{
+	
+	@Value("${sample.param}")
+	private String param;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldController.class);
+	
+	@GetMapping("/hello")
+	public Mono<String> sayHello(ServerHttpRequest request) {
+				return Mono.just("Hello World:"+param);
+		
+	}
+	
+}
